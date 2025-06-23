167b8239b1af21be5e40bdfab73050b86eb6e3c0
albim72 <marcin.albiniak72@gmail.com>
2025-06-23 14:19:48 +0200

    Update simplenn.py

--- DZIEN_1/ProstaSiecNeuronowa/simplenn.py
+++ DZIEN_1/ProstaSiecNeuronowa/simplenn.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 class SimpleNeuralNetwork:
+    # def __new__(cls, *args, **kwargs):
+    #     return super().__new__(SimpleNeuralNetwork)
+
     def __init__(self):
         np.random.seed(1)
         self.weights = 2*np.random.random((3,1))-1
 
-        
+    def __repr__(self):
+        return f"SimpleNeuralNetwork\n({self.weights})"
