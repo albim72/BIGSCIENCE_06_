bad01923680917a82827264f3d69cf4e511b2872
albim72 <marcin.albiniak72@gmail.com>
2025-06-23 14:51:45 +0200

    Update simplenn.py

--- DZIEN_1/ProstaSiecNeuronowa/simplenn.py
+++ DZIEN_1/ProstaSiecNeuronowa/simplenn.py
@@ -10,3 +10,32 @@ class SimpleNeuralNetwork:
 
     def __repr__(self):
         return f"SimpleNeuralNetwork\n({self.weights})"
+
+
+    #funkcja aktywacji - sigmoid:
+    def sigmoid(self, x):
+        return 1/(1+np.exp(-x))
+    
+    #różniczka funkcji aktywacji - sigmoid:
+    def sigmoid_d(self, x):
+        return x*(1-x)
+    
+    #propagacja
+    def propagation(self, inputs):
+        return self.sigmoid(np.dot(inputs.astype(float), self.weights))
+    
+    #propagacja wsteczna
+    def propagation_back(self, propagation_result, train_input,train_output):       
+        error = train_output - propagation_result
+        self.weights += np.dot(train_input.T, error * self.sigmoid_d(propagation_result))
+        
+    #trening
+    def train(self, train_input, train_output, train_iters):
+        for _ in range(train_iters):
+            propagation_result = self.propagation(train_input)
+            self.propagation_back(propagation_result, train_input,train_output) 
+        
+    
+
+    
+
